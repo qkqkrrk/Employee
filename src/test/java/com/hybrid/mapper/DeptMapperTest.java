bfc553ab160472214dfc85a34720106d76d756f6
qkqkrrk <vgb525@hanmail.net>
2016-02-26 12:23:17 +0900

    mapper setting

--- src/test/java/com/hybrid/mapper/DeptMapperTest.java
+++ src/test/java/com/hybrid/mapper/DeptMapperTest.java
@@ -10,7 +10,21 @@ import com.hybrid.model.Dept;
 public class DeptMapperTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("spring/beans_db.xml");
+		
+		DeptMapper mapper = ctx.getBean(DeptMapper.class);
+		
+		List<Dept> depts = mapper.selectAll();
+		
+		for (Dept dept : depts) {
+			System.out.println(dept.getDeptno()+" "+dept.getDname()+" "+dept.getLoc());
+		}
+		
+		ctx.close();
+	}
+	
+	void test1(){
+		
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("spring/beans_db.xml");
 		//SqlSessionTemplate sqlSession = ctx.getBean(SqlSessionTemplate.class);
 		SqlSessionTemplate sqlSession = (SqlSessionTemplate) ctx.getBean("sqlSessionTemplate");
